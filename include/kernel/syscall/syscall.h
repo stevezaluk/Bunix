167b7e88d7d0888d44ad096f1d780aa9b9ce9fab
0x16000 <openbsd.gnu@gmail.com>
2025-03-08 13:10:27 +0100

    SYS_exit implementation 32-bit

--- include/kernel/syscall/syscall.h
+++ include/kernel/syscall/syscall.h
@@ -5,8 +5,10 @@
 
 // Syscall numbers
 #define SYS_WRITE 4
+#define SYS_EXIT 1
 
 // Function prototype for sys_write
 int sys_write(int fd, const void *buf, size_t count);
+void sys_exit(int status);
 
 #endif // SYSCALL_H
